3e9ffe615155487482a3d447baa32317ac190fd1
Alvaro Olavarria <alvaroolavD2000@gmail.com>
2024-03-01 21:16:30 +0100

    botones personalizados

---
+++ Assets/Scenes/Alvaro/ButtonColorChange.cs
@@ -0,0 +1,43 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonColorChange : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public TextMeshProUGUI buttonText;
+    public Color normalColor;
+    public Color hoverColor;
+    public FontStyles normalFontStyle;
+    public FontStyles hoverFontStyle;
+    public string normalText;
+    public string hoverText;
+
+    private Button button;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        buttonText = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (button != null)
+        {
+            buttonText.color = hoverColor;
+            buttonText.fontStyle = hoverFontStyle;
+            buttonText.text = hoverText;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (button != null)
+        {
+            buttonText.color = normalColor;
+            buttonText.fontStyle = normalFontStyle;
+            buttonText.text = normalText;
+        }
+    }
+}
