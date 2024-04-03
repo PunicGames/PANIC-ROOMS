51a60cf35dc3fa19cbc8c0aa1de248a78346b999
Javier Serrano del Amo <javiserrano97@hotmail.es>
2024-04-03 13:17:08 +0200

    Enemy behavior and lantern
    Enemy behavior is updated. Now it detects player from different parts of the body making it more accurate. Also, some bugs related to behavior are corrected and parameters behavior re adjusted. Finally, lantern behavior is modified.

--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -6,20 +6,25 @@ public class EnemyStats : MonoBehaviour
 {
     [SerializeField]private EnemyBehavior enemy_behavior;
 
-    private float[] health_increase_rate_levels = { 0.0f, 40.0f, 38.0f, 35.0f, 33.0f, 30.0f, 27.0f, 25.0f, 20.0f };
-    private float[] health_decrease_rate_levels = { 0.0f, 20.0f, 25.0f, 29.5f, 35.5f, 37.0f, 40.0f, 47.0f, 55.0f };
-    private float[] catch_distance_levels = { 0.0f, 1.0f, 1.25f, 1.25f, 1.3f, 1.4f, 1.5f, 1.6f, 1.7f };
-    private float[] enemy_speed_levels = { 0.0f, 0.5f, 0.7f, 0.8f, 1.0f, 1.2f, 1.3f, 1.5f, 1.6f };
-    private float[] teleportation_chance_levels = { 0.0f, 1.0f, 0.8f, 0.65f, 0.6f, 0.5f, 0.45f, 0.4f, 0.35f };
-    private float[] teleport_distance_levels = { 0.0f, 35.0f, 20.0f, 16.0f, 12.5f, 10.0f, 9.0f, 8.0f, 7.5f };
+    private float[] health_increase_rate_levels = { 0.0f, 40.0f, 38.0f, 35.00f, 33.0f, 30.0f, 27.00f, 25.0f, 20.00f };
+    private float[] health_decrease_rate_levels = { 0.0f, 20.0f, 22.0f, 25.50f, 29.5f, 32.0f, 35.00f, 37.0f, 40.00f };
+    private float[] catch_distance_levels =       { 0.0f,  0.7f,  0.8f, 0.9f,  1.0f,  1.05f,  1.15f,  1.3f,  1.40f };
+    private float[] enemy_speed_levels =          { 0.0f,  0.4f,  0.45f,  0.50f,  0.55f,  0.65f,  0.70f,  0.75f,  0.80f };
+    private float[] teleportation_chance_levels = { 0.0f,  1.0f,  0.9f,  0.8f,  0.7f,  0.65f,  0.6f,  0.5f,  0.5f };
+    private float[] teleport_distance_levels =    { 0.0f, 35.0f, 20.0f, 16.00f, 12.5f, 10.0f,  9.00f,  8.0f,  7.50f };
+    private float[] teleport_timer_levels =       { 5.0f,  5.0f,  4.5f,  4.00f,  3.0f,  2.5f,  2.00f,  1.5f,  0.75f };
 
 
     public void UpdateStats(int player_points) {
-        enemy_behavior.SetHealthIncreaseRate(health_increase_rate_levels[player_points]);
-        enemy_behavior.SetHealthDecreaseRate(health_decrease_rate_levels[player_points]);
-        enemy_behavior.SetCatchDistance(catch_distance_levels[player_points]);
-        enemy_behavior.SetSpeed(enemy_speed_levels[player_points]);
-        enemy_behavior.SetTeleportationChance(teleportation_chance_levels[player_points]);
-        enemy_behavior.SetTeleportDistance(teleport_distance_levels[player_points]);
+
+        int upgrade_level = (player_points >= health_increase_rate_levels.Length) ? (health_increase_rate_levels.Length - 1) : player_points;
+
+        enemy_behavior.SetHealthIncreaseRate(health_increase_rate_levels[upgrade_level]);
+        enemy_behavior.SetHealthDecreaseRate(health_decrease_rate_levels[upgrade_level]);
+        enemy_behavior.SetCatchDistance(catch_distance_levels[upgrade_level]);
+        enemy_behavior.SetSpeed(enemy_speed_levels[upgrade_level]);
+        enemy_behavior.SetTeleportationChance(teleportation_chance_levels[upgrade_level]);
+        enemy_behavior.SetTeleportDistance(teleport_distance_levels[upgrade_level]);
+        enemy_behavior.SetTeleportTimer(teleport_timer_levels[upgrade_level]);
     }
 }
