9c42736f6a7b3bad14a8808405ac0fb6040844d3
Javier Serrano del Amo <javiserrano97@hotmail.es>
2024-04-30 19:05:45 +0200

    adjusted difficulty for enemy

--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -6,13 +6,13 @@ public class EnemyStats : MonoBehaviour
 {
     [SerializeField]private EnemyBehavior enemy_behavior;
 
-    private float[] health_increase_rate_levels = { 0.0f, 40.0f, 38.0f, 35.00f, 33.0f, 30.0f, 27.00f, 25.0f, 20.00f };
-    private float[] health_decrease_rate_levels = { 0.0f, 20.0f, 22.0f, 25.50f, 29.5f, 32.0f, 35.00f, 37.0f, 40.00f };
-    private float[] catch_distance_levels =       { 0.0f,  0.7f,  0.8f, 0.9f,  1.0f,  1.05f,  1.15f,  1.3f,  1.40f };
-    private float[] enemy_speed_levels =          { 0.0f,  0.4f,  0.45f,  0.50f,  0.55f,  0.65f,  0.70f,  0.75f,  0.80f };
-    private float[] teleportation_chance_levels = { 0.0f,  1.0f,  0.9f,  0.8f,  0.7f,  0.65f,  0.6f,  0.5f,  0.5f };
-    private float[] teleport_distance_levels =    { 0.0f, 35.0f, 20.0f, 16.00f, 12.5f, 10.0f,  9.00f,  8.0f,  7.50f };
-    private float[] teleport_timer_levels =       { 5.0f,  5.0f,  4.5f,  4.00f,  3.0f,  2.5f,  2.00f,  1.5f,  0.75f };
+    private float[] health_increase_rate_levels = { 0.0f, 40.00f, 37.00f, 33.00f, 30.00f, 27.00f, 23.00f, 20.00f, 20.00f };
+    private float[] health_decrease_rate_levels = { 0.0f,  6.00f,  7.00f,  8.50f,  9.50f, 10.00f, 12.00f, 13.00f, 13.00f };
+    private float[] catch_distance_levels =       { 0.0f,  0.70f,  0.80f,  0.90f,  1.00f,  1.10f,  1.20f,  1.30f,  1.30f };
+    private float[] enemy_speed_levels =          { 0.0f,  0.40f,  0.45f,  0.50f,  0.55f,  0.60f,  0.70f,  0.75f,  0.75f };
+    private float[] teleportation_chance_levels = { 0.0f,  1.00f,  0.90f,  0.80f,  0.70f,  0.60f,  0.50f,  0.40f,  0.40f };
+    private float[] teleport_distance_levels =    { 0.0f, 35.00f, 31.00f, 26.00f, 22.00f, 18.00f, 13.00f,  8.00f,  8.00f };
+    private float[] teleport_timer_levels =       { 0.0f,  4.00f,  3.60f,  3.20f,  2.75f,  2.20f,  1.80f,  1.50f,  1.50f };
 
 
     public void UpdateStats(int player_points) {
